a734927b9304bd2f394ab3d655e9bd851852c2d3
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-12-06 02:33:05 -0300

    Conectamysql

--- conect/conectaMysql.php
+++ conect/conectaMysql.php
@@ -2,26 +2,32 @@
 
 $sslCertPath = '/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem'; 
 
-// Parâmetros de conexão com o MySQL
+// Parâmetros de conexão com o MySQL a partir das variáveis de ambiente
 $host = $_ENV["AZURE_MYSQL_HOST"];
 $username = $_ENV["AZURE_MYSQL_USERNAME"];
 $password = $_ENV["AZURE_MYSQL_PASSWORD"];
 $dbname = $_ENV["AZURE_MYSQL_DBNAME"];
 
-// Configurando a conexão MySQL com SSL
-$conMysql = mysqli_init(); // Inicializando a conexão
-mysqli_ssl_set($conMysql, NULL, NULL, $sslCertPath, NULL, NULL); 
-mysqli_real_connect($conMysql, $host, $username, $password, $dbname);
+// Verificando se as variáveis de ambiente estão configuradas corretamente
+if (empty($host) || empty($username) || empty($password) || empty($dbname)) {
+    die("Missing environment variables.");
+}
 
-if (mysqli_connect_errno()) {
-    // Caso haja erro na conexão, exibe uma mensagem
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+// Inicializando a conexão MySQL
+$conMysql = mysqli_init();
+
+// Configurando o SSL
+mysqli_ssl_set($conMysql, NULL, NULL, $sslCertPath, NULL, NULL);
+
+// Tentando realizar a conexão
+if (!mysqli_real_connect($conMysql, $host, $username, $password, $dbname)) {
+    // Logando o erro em vez de exibir no navegador
+    error_log("MySQL Connection Error: " . mysqli_connect_error());
+    echo "Error: Unable to connect to the database." . PHP_EOL;
     exit;
 }
 
-// Configurando o charset para utf8
+// Configurando o charset para UTF-8
 mysqli_set_charset($conMysql, 'utf8');
 
 ?>
