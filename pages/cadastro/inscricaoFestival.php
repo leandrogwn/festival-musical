02aaadb781928e73ff552d5202fa242d6611e079
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-12-11 22:55:24 -0300

    Teste

--- pages/cadastro/inscricaoFestival.php
+++ pages/cadastro/inscricaoFestival.php
@@ -5,7 +5,7 @@ if ( !isset( $_SESSION ) ) {
     session_start();
     $_SESSION[ 'tab' ] = 'inscrito';
 }
-include ('../../conectaMysqlOO.php');
+include ('../../../conectaMysqlOO.php');
 
 class inscricaoFestival {
 
